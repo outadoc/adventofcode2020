7ef789ca033c3f7dd4cb056aae2f11c8a746dc74
Baptiste Candellier <outadoc@gmail.com>
2020-12-06 14:35:12 +0100

    Simplify reader

--- common/src/jvmMain/kotlin/fr/outadoc/aoc/scaffold/FileReader.kt
+++ common/src/jvmMain/kotlin/fr/outadoc/aoc/scaffold/FileReader.kt
@@ -3,8 +3,7 @@ package fr.outadoc.aoc.scaffold
 actual class FileReader {
     actual fun readInput(filename: String): String {
         return this::class.java.classLoader!!.let { loader ->
-            loader.getResourceAsStream(filename)!!
-                .reader().readText()
+            loader.getResource(filename)!!.readText()
         }
     }
 }
