caab3ddaa408cca62b8091f455c8a373e1fcab3c
Baptiste Candellier <outadoc@gmail.com>
2020-12-08 20:32:14 +0100

    Don't make method inline

--- common/src/commonMain/kotlin/fr/outadoc/aoc/scaffold/Day.kt
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/scaffold/Day.kt
@@ -1,11 +1,11 @@
 package fr.outadoc.aoc.scaffold
 
-abstract class Day(val year: Year) {
+abstract class Day(private val year: Year) {
 
     abstract fun step1(): Long
     abstract fun step2(): Long
 
-    protected inline fun <reified T> T.readDayInput(): String {
+    protected fun <T> T.readDayInput(): String {
         val year = year.id
         val day = this!!::class.simpleName!!
         return FileReader().readInput("$year/$day.txt")
