169e13775aa0316619dcc49ddfa118456ddb469a
Baptiste Candellier <outadoc@gmail.com>
2020-12-09 21:41:05 +0100

    Read file (posix)

--- common/src/linuxX64Main/kotlin/fr/outadoc/aoc/scaffold/FileReader.kt
+++ common/src/linuxX64Main/kotlin/fr/outadoc/aoc/scaffold/FileReader.kt
@@ -1,13 +1,33 @@
 package fr.outadoc.aoc.scaffold
 
+import kotlinx.cinterop.*
+import platform.posix.*
+
 actual class FileReader {
 
     companion object {
         const val BASE_DIR = "src/commonTest/resources"
     }
 
-    actual fun readInput(filename: String): String {
+    actual fun readInput(filename: String): String = memScoped {
         val path = "$BASE_DIR/$filename"
-        TODO()
+
+        // Open file
+        val f = fopen(path, "r")
+
+        // Count bytes
+        fseek(f, 0, SEEK_END)
+        val fileSize = ftell(f)
+        fseek(f, 0, SEEK_SET)
+
+        // Allocate buffer
+        val buffer = allocArray<ByteVar>(fileSize + 1)
+
+        // Read file to buffer
+        fread(buffer, 1, fileSize.toULong(), f)
+        fclose(f)
+        buffer[fileSize] = 0
+
+        return buffer.toKStringFromUtf8()
     }
 }
