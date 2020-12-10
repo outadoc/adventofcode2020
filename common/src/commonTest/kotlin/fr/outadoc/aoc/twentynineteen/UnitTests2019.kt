928db8a770f54f5a691d09a00b3b8694baf51b63
Baptiste Candellier <outadoc@gmail.com>
2020-12-10 21:33:49 +0100

    Rename tests

--- common/src/commonTest/kotlin/fr/outadoc/aoc/twentynineteen/UnitTests2019.kt
+++ common/src/commonTest/kotlin/fr/outadoc/aoc/twentynineteen/UnitTests2019.kt
@@ -1,27 +1,32 @@
 package fr.outadoc.aoc.twentynineteen
 
+import kotlin.js.JsName
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class UnitTests2019 {
 
     @Test
-    fun testDay1Step1() {
+    @JsName("testDay1Step1")
+    fun `Test day 1 step 1`() {
         assertEquals(3512133, Day1().step1())
     }
 
     @Test
-    fun testDay1Step2() {
+    @JsName("testDay1Step2")
+    fun `Test day 1 step 2`() {
         assertEquals(5265294, Day1().step2())
     }
 
     @Test
-    fun testDay2Step1() {
+    @JsName("testDay2Step1")
+    fun `Test day 2 step 1`() {
         assertEquals(4330636, Day2().step1())
     }
 
     @Test
-    fun testDay2Step2() {
+    @JsName("testDay2Step2")
+    fun `Test day 2 step 2`() {
         assertEquals(60086, Day2().step2())
     }
 }
--- common/src/commonTest/kotlin/fr/outadoc/aoc/twentytwenty/UnitTests2020.kt
+++ common/src/commonTest/kotlin/fr/outadoc/aoc/twentytwenty/UnitTests2020.kt
@@ -1,107 +1,128 @@
 package fr.outadoc.aoc.twentytwenty
 
+import kotlin.js.JsName
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class UnitTests2020 {
 
     @Test
-    fun testDay1Step1() {
+    @JsName("testDay1Step1")
+    fun `Test day 1 step 1`() {
         assertEquals(786811, Day1().step1())
     }
 
     @Test
-    fun testDay1Step2() {
+    @JsName("testDay1Step2")
+    fun `Test day 1 step 2`() {
         assertEquals(199068980, Day1().step2())
     }
 
     @Test
-    fun testDay2Step1() {
+    @JsName("testDay2Step1")
+    fun `Test day 2 step 1`() {
         assertEquals(591, Day2().step1())
     }
 
     @Test
-    fun testDay2Step2() {
+    @JsName("testDay2Step2")
+    fun `Test day 2 step 2`() {
         assertEquals(335, Day2().step2())
     }
 
     @Test
-    fun testDay3Step1() {
+    @JsName("testDay3Step1")
+    fun `Test day 3 step 1`() {
         assertEquals(276, Day3().step1())
     }
 
     @Test
-    fun testDay3Step2() {
+    @JsName("testDay3Step2")
+    fun `Test day 3 step 2`() {
         assertEquals(7812180000, Day3().step2())
     }
 
     @Test
-    fun testDay4Step1() {
+    @JsName("testDay4Step1")
+    fun `Test day 4 step 1`() {
         assertEquals(260, Day4().step1())
     }
 
     @Test
-    fun testDay4Step2() {
+    @JsName("testDay4Step2")
+    fun `Test day 4 step 2`() {
         assertEquals(153, Day4().step2())
     }
 
     @Test
-    fun testDay5Step1() {
+    @JsName("testDay5Step1")
+    fun `Test day 5 step 1`() {
         assertEquals(915, Day5().step1())
     }
 
     @Test
-    fun testDay5Step2() {
+    @JsName("testDay5Step2")
+    fun `Test day 5 step 2`() {
         assertEquals(699, Day5().step2())
     }
 
     @Test
-    fun testDay6Step1() {
+    @JsName("testDay6Step1")
+    fun `Test day 6 step 1`() {
         assertEquals(6273, Day6().step1())
     }
 
     @Test
-    fun testDay6Step2() {
+    @JsName("testDay6Step2")
+    fun `Test day 6 step 2`() {
         assertEquals(3254, Day6().step2())
     }
 
     @Test
-    fun testDay7Step1() {
+    @JsName("testDay7Step1")
+    fun `Test day 7 step 1`() {
         assertEquals(332, Day7().step1())
     }
 
     @Test
-    fun testDay7Step2() {
+    @JsName("testDay7Step2")
+    fun `Test day 7 step 2`() {
         assertEquals(10875, Day7().step2())
     }
 
     @Test
-    fun testDay8Step1() {
+    @JsName("testDay8Step1")
+    fun `Test day 8 step 1`() {
         assertEquals(1528, Day8().step1())
     }
 
     @Test
-    fun testDay8Step2() {
+    @JsName("testDay8Step2")
+    fun `Test day 8 step 2`() {
         assertEquals(640, Day8().step2())
     }
 
     @Test
-    fun testDay9Step1() {
+    @JsName("testDay9Step1")
+    fun `Test day 9 step 1`() {
         assertEquals(22477624, Day9().step1())
     }
 
     @Test
-    fun testDay9Step2() {
+    @JsName("testDay9Step2")
+    fun `Test day 9 step 2`() {
         assertEquals(2980044, Day9().step2())
     }
 
     @Test
-    fun testDay10Step1() {
+    @JsName("testDay10Step1")
+    fun `Test day 10 step 1`() {
         assertEquals(1885, Day10().step1())
     }
 
     @Test
-    fun testDay10Step2() {
+    @JsName("testDay10Step2")
+    fun `Test day 10 step 2`() {
         assertEquals(2024782584832, Day10().step2())
     }
 }
