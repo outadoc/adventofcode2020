8e45096d6e5d09a61f5882f823281aae5362e453
Baptiste Candellier <outadoc@gmail.com>
2020-12-14 18:33:44 +0100

    Day 14 (#6)

---
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day14.kt
@@ -0,0 +1,100 @@
+package fr.outadoc.aoc.twentytwenty
+
+import fr.outadoc.aoc.scaffold.Day
+import fr.outadoc.aoc.scaffold.Year
+
+class Day14 : Day(Year.TwentyTwenty) {
+
+    companion object {
+        private val maskRegex = Regex("^mask = ([01X]+)$")
+        private val setRegex = Regex("^mem\\[([0-9]+)] = ([0-9]+)$")
+    }
+
+    private val actions: Sequence<Action> =
+        readDayInput()
+            .lineSequence()
+            .map { line ->
+                val maskRes = maskRegex.find(line)
+                val setRes = setRegex.find(line)
+
+                when {
+                    maskRes != null -> Action.SetMask(maskRes.groupValues[1])
+                    setRes != null -> Action.SetValue(
+                        addr = setRes.groupValues[1].toLong(),
+                        value = setRes.groupValues[2].toLong()
+                    )
+                    else -> throw IllegalArgumentException()
+                }
+            }
+
+    private data class Mask(val mask: String) {
+
+        private fun Long.setBit(n: Int): Long = this or (1L shl n)
+        private fun Long.clearBit(n: Int): Long = this and (1L shl n).inv()
+
+        private val reverseMask: String = mask.reversed()
+
+        fun maskValue(value: Long): Long =
+            reverseMask.foldIndexed(value) { n: Int, acc: Long, bit: Char ->
+                when (bit) {
+                    '0' -> acc.clearBit(n)
+                    '1' -> acc.setBit(n)
+                    else -> acc
+                }
+            }
+
+        fun maskAddress(addr: Long): List<Long> =
+            reverseMask.foldIndexed(listOf(addr)) { n: Int, acc: List<Long>, bit: Char ->
+                when (bit) {
+                    '0' -> acc
+                    '1' -> acc.map { addr -> addr.setBit(n) }
+                    else -> acc.map { addr ->
+                        listOf(
+                            addr.setBit(n),
+                            addr.clearBit(n)
+                        )
+                    }.flatten()
+                }
+            }
+    }
+
+    private data class State(
+        val mask: Mask = Mask(""),
+        val memory: Map<Long, Long> = mapOf()
+    )
+
+    private sealed class Action {
+        data class SetMask(val mask: String) : Action()
+        data class SetValue(val addr: Long, val value: Long) : Action()
+    }
+
+    private fun State.reduceV1(action: Action): State = when (action) {
+        is Action.SetMask -> copy(mask = Mask(action.mask))
+        is Action.SetValue -> copy(memory = memory.toMutableMap().apply {
+            put(action.addr, mask.maskValue(action.value))
+        })
+    }
+
+    private fun State.reduceV2(action: Action): State = when (action) {
+        is Action.SetMask -> copy(mask = Mask(action.mask))
+        is Action.SetValue -> copy(memory = memory.toMutableMap().apply {
+            mask.maskAddress(action.addr).forEach { addr ->
+                put(addr, action.value)
+            }
+        })
+    }
+
+    override fun step1(): Long {
+        return actions
+            .fold(State()) { acc, action -> acc.reduceV1(action) }
+            .memory.values
+            .sum()
+    }
+
+    override fun step2(): Long {
+        return actions
+            .fold(State()) { acc, action -> acc.reduceV2(action) }
+            .memory.values
+            .sum()
+    }
+}
--- common/src/commonTest/kotlin/fr/outadoc/aoc/twentytwenty/UnitTests2020.kt
+++ common/src/commonTest/kotlin/fr/outadoc/aoc/twentytwenty/UnitTests2020.kt
@@ -161,4 +161,16 @@ class UnitTests2020 {
     fun `Test day 13 step 2`() {
         assertEquals(840493039281088, Day13().step2())
     }
+
+    @Test
+    @JsName("testDay14Step1")
+    fun `Test day 14 step 1`() {
+        assertEquals(13556564111697, Day14().step1())
+    }
+
+    @Test
+    @JsName("testDay14Step2")
+    fun `Test day 14 step 2`() {
+        assertEquals(4173715962894, Day14().step2())
+    }
 }
