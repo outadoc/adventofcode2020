ee67d3cdadabcba81a8350ef0a4bc6b47d4200ad
Baptiste Candellier <outadoc@gmail.com>
2020-12-20 11:33:33 +0100

    Init day 20

---
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day20.kt
@@ -0,0 +1,15 @@
+package fr.outadoc.aoc.twentytwenty
+
+import fr.outadoc.aoc.scaffold.Day
+import fr.outadoc.aoc.scaffold.Year
+
+class Day20 : Day(Year.TwentyTwenty) {
+
+    override fun step1(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun step2(): Long {
+        TODO("Not yet implemented")
+    }
+}
