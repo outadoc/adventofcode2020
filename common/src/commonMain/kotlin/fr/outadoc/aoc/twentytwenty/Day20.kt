a1c7d7d57a7ab5c63b6ebd9e5d07b04a0d5e2658
Baptiste Candellier <outadoc@gmail.com>
2020-12-20 11:39:23 +0100

    Create model

--- common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day20.kt
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day20.kt
@@ -5,6 +5,13 @@ import fr.outadoc.aoc.scaffold.Year
 
 class Day20 : Day(Year.TwentyTwenty) {
 
+    private data class Position(val x: Int, val y: Int)
+
+    private data class Tile(val id: Int, val content: List<CharArray>)
+
+    private data class TransformationVector(val x: Int, val y: Int)
+    private data class TransformedTile(val tile: Tile, val transform: TransformationVector)
+
     override fun step1(): Long {
         TODO("Not yet implemented")
     }
