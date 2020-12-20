3d8bbf1d8f98fe3e8412bda68e089faa54f3a34e
Baptiste Candellier <outadoc@gmail.com>
2020-12-20 11:45:07 +0100

    Parse tiles

--- common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day20.kt
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day20.kt
@@ -7,10 +7,28 @@ class Day20 : Day(Year.TwentyTwenty) {
 
     private data class Position(val x: Int, val y: Int)
 
-    private data class Tile(val id: Int, val content: List<CharArray>)
+    private data class Tile(val id: Long, val content: List<CharArray>)
 
     private data class TransformationVector(val x: Int, val y: Int)
     private data class TransformedTile(val tile: Tile, val transform: TransformationVector)
+
+    private val tiles: List<Tile> =
+        readDayInput()
+            .split("\n\n")
+            .map { tileSection ->
+                val tileDescription = tileSection.lines()
+                val id = tileDescription
+                    .first()
+                    .replace("Tile ", "")
+                    .replace(":", "")
+                    .toLong()
+
+                val tileContent = tileDescription
+                    .drop(1)
+                    .map { contentLine -> contentLine.toCharArray() }
+
+                Tile(id = id, content = tileContent)
+            }
 
     override fun step1(): Long {
         TODO("Not yet implemented")
