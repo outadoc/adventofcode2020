fda6b7e54cc9479733d7b74953ad48a0826d78ee
Baptiste Candellier <outadoc@gmail.com>
2020-12-24 08:25:13 +0100

    Still goddamn slow

--- common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day23.kt
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day23.kt
@@ -3,7 +3,6 @@ package fr.outadoc.aoc.twentytwenty
 import fr.outadoc.aoc.scaffold.Day
 import fr.outadoc.aoc.scaffold.Year
 import fr.outadoc.aoc.scaffold.max
-import fr.outadoc.aoc.scaffold.min
 
 class Day23 : Day(Year.TwentyTwenty) {
 
@@ -11,7 +10,9 @@ class Day23 : Day(Year.TwentyTwenty) {
         private const val PRINT_DEBUG = false
     }
 
-    private data class State(val cups: List<Int>)
+    private data class State(val cups: List<Int>) {
+        val lookup: Map<Int, Int> = cups.zip(cups.indices).toMap()
+    }
 
     private val initialState: State =
         readDayInput()
@@ -25,21 +26,25 @@ class Day23 : Day(Year.TwentyTwenty) {
         val currentCup = cups.first()
 
         // Pick up 3 cups after the current cup
-        val pickedCups = cups.drop(1).take(3)
+        val pickedCups = cups.subList(1, 4)
 
         // What remains of the cups without the ones we picked up
         val remainingCups = listOf(currentCup) + cups.drop(4)
+        val maxRemainingCup = (cups.size downTo cups.size - 3).first { cup ->
+            cup !in pickedCups
+        }
 
         // Select the destination cup
-        val destinationCup = ((currentCup - 1) downTo remainingCups.min())
-            .firstOrNull { cup -> cup in remainingCups } ?: remainingCups.max()
+        val destinationCup = ((currentCup - 1) downTo 1)
+            .firstOrNull { cup -> cup !in pickedCups } ?: maxRemainingCup
+
+        val destinationCupIndex = lookup.getValue(destinationCup) - 3
 
         // Move cups to the right position
         val withPickedCups: List<Int> =
-            remainingCups.takeWhile { it != destinationCup } +
-                destinationCup +
+            remainingCups.subList(0, destinationCupIndex + 1) +
                 pickedCups +
-                remainingCups.takeLastWhile { it != destinationCup }
+                remainingCups.subList(destinationCupIndex + 1, remainingCups.size)
 
         // Place the current cup at the back of the list
         val final = withPickedCups.drop(1) + withPickedCups.first()
@@ -62,7 +67,8 @@ class Day23 : Day(Year.TwentyTwenty) {
     }
 
     private fun State.nthIteration(n: Int): State {
-        return (0 until n).fold(this) { state, _ ->
+        return (0 until n).foldIndexed(this) { index, state, _ ->
+            println("${index.toFloat() / n.toFloat() * 100f} %")
             state.next()
         }
     }
