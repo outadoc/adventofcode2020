23a1ef8bd6b53e74807bed71c2faf83119faa1e6
Baptiste Candellier <outadoc@gmail.com>
2020-12-26 10:22:20 +0100

    Init day 25

---
+++ common/src/commonMain/kotlin/fr/outadoc/aoc/twentytwenty/Day25.kt
@@ -0,0 +1,15 @@
+package fr.outadoc.aoc.twentytwenty
+
+import fr.outadoc.aoc.scaffold.Day
+import fr.outadoc.aoc.scaffold.Year
+
+class Day25 : Day(Year.TwentyTwenty) {
+
+    fun step1(): Long {
+        TODO()
+    }
+
+    fun step2(): Long {
+        TODO()
+    }
+}
---
+++ common/src/commonTest/kotlin/fr/outadoc/aoc/twentytwenty/Day25Tests.kt
@@ -0,0 +1,20 @@
+package fr.outadoc.aoc.twentytwenty
+
+import kotlin.js.JsName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day25Tests {
+
+    @Test
+    @JsName("testDay25Step1")
+    fun `Test day 25 step 1`() {
+        assertEquals(null, Day25().step1())
+    }
+
+    @Test
+    @JsName("testDay25Step2")
+    fun `Test day 25 step 2`() {
+        assertEquals(null, Day25().step2())
+    }
+}
