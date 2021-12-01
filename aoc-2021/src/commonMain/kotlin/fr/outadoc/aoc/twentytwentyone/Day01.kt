933aa594e28e21888fcf9e1aa8f751ebaabbd54f
Baptiste Candellier <baptiste@candellier.me>
2021-12-01 20:48:46 +0100

    feat: 2021 day 1 (#17)

--- aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
+++ aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
@@ -1,12 +1,30 @@
 package fr.outadoc.aoc.twentytwentyone
 
 import fr.outadoc.aoc.scaffold.Day
+import fr.outadoc.aoc.scaffold.readDayInput
 
 class Day01 : Day<Int> {
 
-    override fun step1(): Int = -1
-    override fun step2(): Int = -1
+    private val input = readDayInput()
+        .lineSequence()
+        .map { it.toInt() }
 
-    override val expectedStep1: Int = -1
-    override val expectedStep2: Int = -1
+    override fun step1() = input
+        .windowed(size = 2, step = 1)
+        .map { it[0] to it[1] }
+        .fold(0) { acc, (prev, current) ->
+            if (current > prev) acc + 1 else acc
+        }
+
+    override fun step2() = input
+        .windowed(size = 3, step = 1)
+        .map { it.sum() }
+        .windowed(size = 2, step = 1)
+        .map { it[0] to it[1] }
+        .fold(0) { acc, (prev, current) ->
+            if (current > prev) acc + 1 else acc
+        }
+
+    override val expectedStep1: Int = 1451
+    override val expectedStep2: Int = 1395
 }
