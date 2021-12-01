e6fb47f0a6e30a40d75abbb6270dd6a2a89813ca
Baptiste Candellier <baptiste@candellier.me>
2021-12-01 20:11:27 +0100

    Step 1

--- aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
+++ aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
@@ -5,15 +5,16 @@ import fr.outadoc.aoc.scaffold.readDayInput
 
 class Day01 : Day<Int> {
 
-    val input = readDayInput()
+    private val input = readDayInput()
         .lineSequence()
         .map { it.toInt() }
 
     override fun step1(): Int {
         return input
-            .drop(1)
-            .fold(0 to input.first()) { (acc, previous), current ->
-                (if (current > previous) acc + 1 else acc) to current
+            .windowed(size = 2, step = 1)
+            .map { it[0] to it[1] }
+            .fold(0) { acc, (prev, current) ->
+                if (current > prev) acc + 1 else acc
             }
     }
 
