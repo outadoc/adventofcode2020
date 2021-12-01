0ca18c97384ecd28f195842ee0bae265de229252
Baptiste Candellier <baptiste@candellier.me>
2021-12-01 20:34:44 +0100

    Allez

--- aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
+++ aoc-2021/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentyone/Day01.kt
@@ -9,25 +9,21 @@ class Day01 : Day<Int> {
         .lineSequence()
         .map { it.toInt() }
 
-    override fun step1(): Int {
-        return input
-            .windowed(size = 2, step = 1)
-            .map { it[0] to it[1] }
-            .fold(0) { acc, (prev, current) ->
-                if (current > prev) acc + 1 else acc
-            }
-    }
+    override fun step1() = input
+        .windowed(size = 2, step = 1)
+        .map { it[0] to it[1] }
+        .fold(0) { acc, (prev, current) ->
+            if (current > prev) acc + 1 else acc
+        }
 
-    override fun step2(): Int {
-        return input
-            .windowed(size = 3, step = 1)
-            .map { it.sum() }
-            .windowed(size = 2, step = 1)
-            .map { it[0] to it[1] }
-            .fold(0) { acc, (prev, current) ->
-                if (current > prev) acc + 1 else acc
-            }
-    }
+    override fun step2() = input
+        .windowed(size = 3, step = 1)
+        .map { it.sum() }
+        .windowed(size = 2, step = 1)
+        .map { it[0] to it[1] }
+        .fold(0) { acc, (prev, current) ->
+            if (current > prev) acc + 1 else acc
+        }
 
     override val expectedStep1: Int = 1451
     override val expectedStep2: Int = 1395
