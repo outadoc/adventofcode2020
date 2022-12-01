1109049655abe20911fda3ac57dbb598cc935b61
Baptiste Candellier <outadoc@gmail.com>
2022-12-01 13:57:58 +0100

    feat: 2022 day 1

--- aoc-2022/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentytwo/Day01.kt
+++ aoc-2022/src/commonMain/kotlin/fr/outadoc/aoc/twentytwentytwo/Day01.kt
@@ -1,5 +1,38 @@
 package fr.outadoc.aoc.twentytwentytwo
 
 import fr.outadoc.aoc.scaffold.Day
+import fr.outadoc.aoc.scaffold.readDayInput
 
-class Day01 : Day<Int>
+class Day01 : Day<Long> {
+
+    private val input: Sequence<Elf> =
+        readDayInput()
+            .splitToSequence("\n\n")
+            .map { elfInput ->
+                Elf(
+                    calories = elfInput.lines()
+                        .map { caloriesStr ->
+                            caloriesStr.toLong()
+                        }
+                )
+            }
+
+    private data class Elf(val calories: List<Long>) {
+        val total: Long = calories.sum()
+    }
+
+    override fun step1(): Long {
+        val elfWithMostCalories: Elf = input.maxBy { elf -> elf.total }
+        return elfWithMostCalories.total
+    }
+
+    override val expectedStep1: Long = 67450
+
+    override fun step2(): Long {
+        val top3Elves: Sequence<Elf> =
+            input.sortedByDescending { elf -> elf.total }.take(3)
+        return top3Elves.sumOf { elf -> elf.total }
+    }
+
+    override val expectedStep2: Long = 199357
+}
